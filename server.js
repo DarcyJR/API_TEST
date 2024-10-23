2b801505be268e178bb94fc25a47ff1ed5956d17
Darcy Junior <73891108+DarcyJR@users.noreply.github.com>
2024-10-23 09:58:38 -0300

    Update server.js
    comentando partes do codigo

--- server.js
+++ server.js
@@ -4,25 +4,30 @@ const app = express();
 const routes = require('./routes');
 
 
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');*/
 
-mongoose.connect(process.env.CONNECTMONGO || process.env.CONNECT_MONGO)
+/*mongoose.connect(process.env.CONNECTMONGO || process.env.CONNECT_MONGO)
     .then(() => {
         console.log('Base conectada');
         app.emit('conectado');
     })
     .catch((e) => {
         console.error(e);
-    });
+    });*/
 
 app.use(express.json());//Analisar o body como json
 app.use(express.urlencoded({ extends: true }));
 
 app.use(routes);
 
-app.on('conectado', () => {
+app.listen(3000, () => {
+        console.log('Servidor iniciado');
+        console.log('http://localhost:3000');
+})
+
+/*app.on('conectado', () => {
     app.listen(3000, () => {
         console.log('Servidor iniciado');
         console.log('http://localhost:3000');
     })
-})
+})*/
