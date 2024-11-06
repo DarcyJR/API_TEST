f67f7e7d7897058d1a6be5b516d15ded8cbed0c0
Darcy Junior <73891108+DarcyJR@users.noreply.github.com>
2024-11-06 16:43:07 -0300

    Update ticketController.js

--- src/controllers/ticketController.js
+++ src/controllers/ticketController.js
@@ -1,29 +1,30 @@
 const TicketModel = require('../models/TicketModels');
 
-exports.homeWebHook = async (req, res) =>{
-    console.log(req.body)
+exports.homeWebHook = async (req, res) => {
+    console.log(req.body);
     try {
         const dados = await TicketModel.create({
             idTicket: req.body[0].objectId
         });
         console.log(dados);
 
-        fetch(`https://api.hubapi.com/crm/v3/objects/tickets/${req.body[0].objectID}`,{
-            method:'PUT',
-            headers:{
-                'Content-Type':application/json
+        fetch(`https://api.hubapi.com/crm/v3/objects/tickets/${req.body[0].objectId}`, {  // Verifique se 'objectId' está correto
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'  // Coloque 'application/json' entre aspas
             },
-            body:JSON.stringfy({
+            body: JSON.stringify({  // Use 'JSON.stringify' em vez de 'JSON.stringfy'
                 "properties": {
-			        "eta": "2024-12-25",
-                })
+                    "eta": "2024-12-25"
+                }
+            })
         })
-            .then(respnse=> response.json())
-            .then(data=> console.log(data))
-            .catch(error => console.log(error))
-        
+            .then(response => response.json())  // Corrija 'respnse' para 'response'
+            .then(data => console.log(data))
+            .catch(error => console.log(error));
+
     } catch (error) {
         console.error(error);
         res.status(500).send(error);
     }
-}
+};
