fcd95f41a5a7bd72eb9b887e3576e03de5aeb1c7
Darcy Junior <73891108+DarcyJR@users.noreply.github.com>
2024-11-06 14:32:34 -0300

    Update homeController.js

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,13 +1,10 @@
 const HomeModel = require('../models/HomeModels');
 exports.homeGet = async (req, res) => {
-    /*
     try {
-    const home = await HomeModel.find();
-    res.send(home);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ message: 'Erro ao buscar dados.' });
-  }*/
-    res.send("ola darcy");
- 
+        const home = await HomeModel.find();
+        res.send(home);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: 'Erro ao buscar dados.' });
+    }
 }
