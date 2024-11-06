07c2ca4549807553337868ded32955da504533f9
Darcy Junior <73891108+DarcyJR@users.noreply.github.com>
2024-11-06 14:44:51 -0300

    Update ticketController.js

--- src/controllers/ticketController.js
+++ src/controllers/ticketController.js
@@ -12,19 +12,3 @@ exports.homeWebHook = async (req, res) =>{
         res.status(500).send(error);
     }
 }
-/*
-
-exports.homePost = async (req, res) => {
-    await HomeModel.create({
-        titulo: req.body.titulo,
-        descricao: req.body.descricao
-    })
-        .then((dados) => {
-            console.log(dados);
-            res.send(dados);
-        })
-        .catch((e) => {
-            console.log(e);
-            res.send(e);
-        })
-}*/
