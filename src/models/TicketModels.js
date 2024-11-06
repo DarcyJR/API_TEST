cb96d6fec7dcdb28d9efe798bf24101aef5ac1e0
Darcy Junior <73891108+DarcyJR@users.noreply.github.com>
2024-11-06 12:14:01 -0300

    Update TicketModels.js

--- src/models/TicketModels.js
+++ src/models/TicketModels.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 const TicketSchema = new mongoose.Schema({
     idTicket: Int
 })
 
 const TicketModel = mongoose.model('ticket', TicketSchema);
 
-module.exports = TicketModel;
+module.exports = TicketModel;*/
