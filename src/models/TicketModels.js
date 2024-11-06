7ae37a6db7ad52a83d43745a2e2a82d14a1b6e0c
Darcy Junior <73891108+DarcyJR@users.noreply.github.com>
2024-11-06 17:51:14 -0300

    Update TicketModels.js
    Modificação dos parametros para o campo

--- src/models/TicketModels.js
+++ src/models/TicketModels.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 const TicketSchema = new mongoose.Schema({
-    idTicket: Number
+    idTicket: {
+        type:Number,
+        required:true,
+        unique:true
+    }
 })
 
 const TicketModel = mongoose.model('ticket', TicketSchema);
