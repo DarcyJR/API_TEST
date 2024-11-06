edeb1447bd4e695a30d612eddad8b9fcf262e2a1
Darcy Junior <73891108+DarcyJR@users.noreply.github.com>
2024-11-06 16:40:16 -0300

    Update ticketController.js

--- src/controllers/ticketController.js
+++ src/controllers/ticketController.js
@@ -7,7 +7,21 @@ exports.homeWebHook = async (req, res) =>{
             idTicket: req.body[0].objectId
         });
         console.log(dados);
-        res.send(dados);
+
+        fetch(`https://api.hubapi.com/crm/v3/objects/tickets/${req.body[0].objectID}`,{
+            method:'PUT',
+            headers:{
+                'Content-Type':application/json
+            },
+            body:JSON.stringfy({
+                "properties": {
+			        "eta": "2024-11-30",
+                })
+        })
+            .then(respnse=> response.json())
+            .then(data=> console.log(data))
+            .catch(error => console.log(error))
+        
     } catch (error) {
         console.error(error);
         res.status(500).send(error);
