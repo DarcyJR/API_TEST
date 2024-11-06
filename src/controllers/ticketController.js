e86476b3a7c3d2ea74c1cf4ed36df8c7e213b4d2
Darcy Junior <73891108+DarcyJR@users.noreply.github.com>
2024-11-06 12:19:44 -0300

    Update ticketController.js

--- src/controllers/ticketController.js
+++ src/controllers/ticketController.js
@@ -3,9 +3,9 @@ const TicketModel = require('../models/TicketModels');
 exports.homeWebHook = async (req, res) =>{
     console.log(req.body);
     res.status(200).send(res.body);
-    /*await TicketModel.create({
-        idTicket: req.
-    })*/
+    await TicketModel.create({
+        idTicket: req.body.objectId
+    })
 }
 
 /*
